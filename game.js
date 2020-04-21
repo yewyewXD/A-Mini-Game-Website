8152013e162879bf5ea902ecd2596dbba3504dd2
Yew Kang Wei <yewyew6933@gmail.com>
2020-04-21 16:33:56 +0800

    scripting file

---
+++ game.js
@@ -0,0 +1,21 @@
+overlay = document.getElementById('overlay');
+closebtn = document.getElementById('closebtn');
+overlist = document.getElementById('overlay-list');
+openbtn = document.getElementById('openbtn');
+
+closebtn.addEventListener('click', () => {
+  overlay.style.height = "0px";
+  closebtn.style.display = 'none';
+  overlist.style.display = 'none';
+})
+
+openbtn.addEventListener('click', () => {
+  overlay.style.height = '150px';
+  setTimeout(() => {
+    closebtn.style.display = 'inline';
+    overlist.style.display = 'block';
+  }, 300)
+})
+
+
+
