abeabc024c71356b9c583d708c4c7cf67cc33d73
Yew Kang Wei <yewyew6933@gmail.com>
2020-05-03 11:59:18 +0800

    fix @media

--- global.js
+++ global.js
@@ -1,27 +1,32 @@
 //GLOBAL SETTINGS
-const overlay = document.getElementById('overlay');
-const closebtn = document.getElementById('closebtn');
-const overlist = document.getElementById('overlay-list');
-const openbtn = document.getElementById('openbtn');
-const tags = document.querySelectorAll('a');
+const overlay = document.getElementById("overlay");
+const closebtn = document.getElementById("closebtn");
+const overlist = document.getElementById("overlay-list");
+const openbtn = document.getElementById("openbtn");
+const tags = document.querySelectorAll("a");
 
 //menu drop down
-closebtn.addEventListener('click', () => {
+closebtn.addEventListener("click", () => {
   overlay.style.height = "0px";
-  closebtn.style.display = 'none';
-  overlist.style.display = 'none';
-})
+  closebtn.style.display = "none";
+  overlist.style.display = "none";
+});
 
-openbtn.addEventListener('click', () => {
-  overlay.style.height = '200px';
+openbtn.addEventListener("click", () => {
+  overlay.style.height = "200px";
+  if (overlay.clientWidth >= 768) {
+    overlay.style.height = "300px";
+  } else if (overlay.clientWidth >= 360) {
+    overlay.style.height = "200px";
+  }
   setTimeout(() => {
-    closebtn.style.display = 'inline';
-    overlist.style.display = 'block';
-  }, 300)
+    closebtn.style.display = "inline";
+    overlist.style.display = "block";
+  }, 300);
 });
 
 //click and href
-branding = document.getElementById('branding');
-branding.addEventListener('click', () => {
-  location.href = 'home.html';
-})
+branding = document.getElementById("branding");
+branding.addEventListener("click", () => {
+  location.href = "home.html";
+});
