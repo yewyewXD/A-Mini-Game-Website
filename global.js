9025951ff109a0267cb1200d9e89c5e51af6cecc
yewyewXD <yewyew6933@hotmail.com>
2025-02-13 14:58:55 +0800

    catch form submission

--- global.js
+++ global.js
@@ -1,32 +1,41 @@
 //GLOBAL SETTINGS
-const overlay = document.getElementById("overlay");
-const closebtn = document.getElementById("closebtn");
-const overlist = document.getElementById("overlay-list");
-const openbtn = document.getElementById("openbtn");
-const tags = document.querySelectorAll("a");
+const overlay = document.getElementById('overlay')
+const closebtn = document.getElementById('closebtn')
+const overlist = document.getElementById('overlay-list')
+const openbtn = document.getElementById('openbtn')
+const tags = document.querySelectorAll('a')
+const contactForm = document.getElementById('contact-form')
 
 //menu drop down
-closebtn.addEventListener("click", () => {
-  overlay.style.height = "0px";
-  closebtn.style.display = "none";
-  overlist.style.display = "none";
-});
+closebtn.addEventListener('click', () => {
+  overlay.style.height = '0px'
+  closebtn.style.display = 'none'
+  overlist.style.display = 'none'
+})
 
-openbtn.addEventListener("click", () => {
-  overlay.style.height = "200px";
+openbtn.addEventListener('click', () => {
+  overlay.style.height = '200px'
   if (overlay.clientWidth >= 768) {
-    overlay.style.height = "300px";
+    overlay.style.height = '300px'
   } else if (overlay.clientWidth >= 360) {
-    overlay.style.height = "200px";
+    overlay.style.height = '200px'
   }
   setTimeout(() => {
-    closebtn.style.display = "inline";
-    overlist.style.display = "block";
-  }, 300);
-});
+    closebtn.style.display = 'inline'
+    overlist.style.display = 'block'
+  }, 300)
+})
 
 //click and href
-branding = document.getElementById("branding");
-branding.addEventListener("click", () => {
-  location.href = "home.html";
-});
+branding = document.getElementById('branding')
+branding.addEventListener('click', () => {
+  location.href = 'home.html'
+})
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  contactForm.reset?.()
+  alert(
+    `It's a prank! This website can't submit a form. Please email me at hello@kangwei.dev instead.`
+  )
+})
